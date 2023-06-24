f53815bf276c216697989c345288db267c747211
Dmitry Petrochenkov <p.dmitry.y@gmail.com>
2023-06-25 02:06:26 +0500

    feat: add default user and currentDir login

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
-const userName = process.argv.slice(2).reduce((acc, val, i, arr)=>{
+let userName = process.argv.slice(2).reduce((acc, val, i, arr)=>{
   if(val.startsWith('--username=')){
       acc.push(`${val.slice(11)}`);
   }
   return acc;
 }, []).join(' ');
 
+if (userName === '') {userName = 'Default user'};
+
 console.log(`Welcome to the File Manager, ${userName}!`);
 
-// You are currently in path_to_working_directory
 
+const currentDir = async () => {
+  console.log(`You are currently in ${process.cwd()}`);
+};
+currentDir()
 
 rl.on("close", () => {
     console.log(`\r\nThank you for using File Manager, ${userName}, goodbye!`);
