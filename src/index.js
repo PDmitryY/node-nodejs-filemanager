db2ff69a3e96ca207f40341017976b3ed09402a2
Dmitry Petrochenkov <p.dmitry.y@gmail.com>
2023-06-25 01:57:42 +0500

    feat: add username recognition

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
-console.log(`Welcome to the File Manager, Username!`);
+const userName = process.argv.slice(2).reduce((acc, val, i, arr)=>{
+  if(val.startsWith('--username=')){
+      acc.push(`${val.slice(11)}`);
+  }
+  return acc;
+}, []).join(' ');
+
+console.log(`Welcome to the File Manager, ${userName}!`);
 
 // You are currently in path_to_working_directory
 
 
 rl.on("close", () => {
-    console.log(`\r\nThank you for using File Manager, Username, goodbye!`);
+    console.log(`\r\nThank you for using File Manager, ${userName}, goodbye!`);
     process.exit(0);
   });
