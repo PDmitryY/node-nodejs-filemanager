d0adec5d966314402712222ef8a4415891996b9f
Dmitry Petrochenkov <p.dmitry.y@gmail.com>
2023-06-25 22:48:17 +0500

    feat: add "cd" function

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import { homedir } from 'node:os';
 import { list } from './modules/list.js';
 import { create } from './modules/create.js'
 import { goUp } from './modules/goUp.js'
+import { changeDir } from './modules/changeDir.js';
 
 const rl = readline.createInterface({ input, output });
 
 const args = process.argv.slice(2);
 let userName = args.reduce((acc, val, i, arr)=>{
-  if(val.startsWith('--username=')){
-      acc.push(`${val.slice(11)}`);
-  }
-  return acc;
+	if(val.startsWith('--username=')){
+		acc.push(`${val.slice(11)}`);
+	}
+	return acc;
 }, []).join(' ');
 
 if (userName === '') {userName = 'Default user'};
@@ -22,42 +23,46 @@ console.log(`Welcome to the File Manager, ${userName}!`);
 process.chdir(homedir())
 
 const currentDir = async () => {
-  console.log(`You are currently in ${process.cwd()}`);
+  	console.log(`You are currently in ${process.cwd()}`);
 };
-currentDir()
+currentDir();
 
 const commandsMap = {
-  // cat: ,
-  add: create,
-  // rn: ,
-  // rm: ,
-  // os: ,
-  // cp: ,
-  // mv: ,
-  // hash: ,
-  // compress: ,
-  // decompress: ,
-  ls: list,
-  up: goUp,
-  // cd: ,
-  ".exit": () => {
-    rl.close(),
-    process.exit(0);
-  },
+	// cat: ,
+	add: create,
+	// rn: ,
+	// rm: ,
+	// os: ,
+	// cp: ,
+	// mv: ,
+	// hash: ,
+	// compress: ,
+	// decompress: ,
+	ls: list,
+	up: goUp,
+	cd: changeDir,
+	".exit": () => {
+		rl.close(),
+		process.exit(0);
+	},
 };
 
 export const lineParser = async (str) => {
-  const [input, ...args] = str.trim().split(' ');
-  // console.log("input", input);
-  Object.entries(commandsMap).map((entr)=>{if(input == entr[0]) {
-    // console.log("entr", entr);
-    entr[1](args);
-  }})
+	const [input, ...args] = str.trim().split(' ');
+	// console.log("input", input);
+	if(commandsMap.hasOwnProperty(input)){
+		Object.entries(commandsMap).map((entr)=>{if(input == entr[0]) {
+		// console.log("entr", entr);
+		entr[1](args);
+		}});
+	} else {
+		console.log("Operation failed\r\n");
+	};
 };
 
 rl.on('line', async (input) => {
-  await lineParser(input);
-  currentDir();
+	await lineParser(input);
+	currentDir();
 });
 
 rl.on("close", () => {
