efef4790e03c645a3670772cdccb1c12d08ab317
Dmitry Petrochenkov <p.dmitry.y@gmail.com>
2023-06-25 07:59:53 +0500

    feat: add ls command and closing on .exit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import { homedir } from 'node:os';
+import { list } from './modules/list.js';
 
 const rl = readline.createInterface({ input, output });
 
@@ -14,11 +16,46 @@ if (userName === '') {userName = 'Default user'};
 
 console.log(`Welcome to the File Manager, ${userName}!`);
 
+process.chdir(homedir())
 
 const currentDir = async () => {
   console.log(`You are currently in ${process.cwd()}`);
 };
 currentDir()
+
+const commandsMap = {
+  // cat: readFile,
+  // add: addFile,
+  // rn: renameFile,
+  // rm: removeFile,
+  // os: handleOs,
+  // cp: cpFile,
+  // mv: moveFile,
+  // hash: handleHash,
+  // compress: handleCompress,
+  // decompress: handleDecompress,
+  ls: list,
+  // up: handleUp,
+  // cd: handleCd,
+  ".exit": () => {
+    rl.close(),
+    process.exit(0);
+  },
+};
+
+export const lineParser = async (str) => {
+  const input = str.trim();
+  // console.log("input", input);
+  Object.entries(commandsMap).map((entr)=>{if(input == entr[0]) {
+    // console.log("entr", entr);
+    entr[1]();
+  }})
+};
+
+rl.on('line', async (input) => {
+  await lineParser(input);
+  currentDir();
+});
 
 rl.on("close", () => {
     console.log(`\r\nThank you for using File Manager, ${userName}, goodbye!`);
